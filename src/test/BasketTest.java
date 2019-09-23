273342980bafe4156e74d8e202cd7822feffbc05
jakub <jakubstankowski@protonmail.com>
2019-09-23 21:48:27 +0200

    Refactoring unit test

--- src/test/BasketTest.java
+++ src/test/BasketTest.java
@@ -1,30 +1,35 @@
 package test;
 
 import com.company.Basket;
+import org.junit.jupiter.api.DisplayName;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BasketTest {
 
+
+    private final Basket basket = new Basket();
+
+
     @org.junit.jupiter.api.Test
+    @DisplayName("test add item to basket")
     void addItemToBasket() {
-        Basket basket = new Basket();
         basket.addItemToBasket("chleb", 1);
         assertTrue(basket.isInBasket("chleb", 1));
     }
 
     @org.junit.jupiter.api.Test
+    @DisplayName("test delete item to basket")
     void deleteFromBasket() {
-        Basket basket = new Basket();
         basket.addItemToBasket("chleb", 1);
         basket.deleteFromBasket("chleb");
         assertFalse(basket.isInBasket("chleb", 1));
     }
 
     @org.junit.jupiter.api.Test
+    @DisplayName("test total basket count")
     void totalBasketCount() {
-        Basket basket = new Basket();
         basket.addItemToBasket("chleb", 1);
         basket.addItemToBasket("sousages", 3);
         int expectSum  = 4;
